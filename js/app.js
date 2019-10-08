7c961e5cac3c1b2f155114826e38cc15404c4dfb
obnoxiousnerd <pdkarawale31@gmail.com>
2019-10-08 12:16:00 +0530

    added prediction

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ $('document').ready(()=>{
 Ω('icon-holder').register(object);
 Ω('temp-holder').register(div);
 Ω('geo-btn').register(button);
+Ω('we-predict').register(p);
 var typeIn =  document.querySelector('#type-in');
 var btn =  Ω('#submit-btn');
 var temp =  Ω('#temp-data');
@@ -19,7 +20,7 @@ var condition =  Ω('#weather-con');
 var icon =  Ω('#icon-holder');
 var sign =  Ω('#sign');
 var geobtn = Ω('#geo-btn')
-
+var predict = Ω('#we-predict');
 
 Ω('#submit-btn').on('click', ()=>{
     getWeather();
@@ -44,7 +45,9 @@ var city =  Ω('#city-name');
 var condition =  Ω('#weather-con');
 var icon =  Ω('#icon-holder');
 var sign =  Ω('#sign');
+var predict = Ω('#we-predict');
     const findcoords = 'https://nominatim.openstreetmap.org/search/'+cityname+'?format=json&addressdetails=1&limit=1&polygon_svg=0';
+    console.log(findcoords);
     $.getJSON(findcoords, (res)=>{
         if(JSON.stringify(res)=='[]'){
             city.html('Not Found');
@@ -66,6 +69,8 @@ var sign =  Ω('#sign');
         city.html(res[0].address.town+', '+res[0].address.country);
         else if(res[0].address.peak != null)
         city.html(res[0].address.peak+', '+res[0].address.country);
+        else
+        city.html(res[0].display_name);
         const prooxy = 'https://cors-anywhere.herokuapp.com/'
     const url = prooxy+'https://api.darksky.net/forecast/'+keey+'/'+lat+','+lon+'?units=si';
     $.getJSON(url, (res)=>{
@@ -81,6 +86,7 @@ var sign =  Ω('#sign');
                 });
                 sign.html('C');
                 condition.html(res.currently.summary);
+                predict.html(res.hourly.summary);
                 iconify(res.currently.icon);
                 humidity.html(parseFloat(res.currently.humidity)*100+'%');
     })
@@ -101,13 +107,16 @@ const getGeoWeather= () => {
         var temp =  Ω('#temp-data');
         var humidity =  Ω('#humid-data');
         var city =  Ω('#city-name');
+        const keey = 'f672ff13193bfcc40427a678ebfdbc71';
         var condition =  Ω('#weather-con');
         var icon =  Ω('#icon-holder');
+        const prooxy = 'https://cors-anywhere.herokuapp.com/'
+        var predict = Ω('#we-predict');
         var sign =  Ω('#sign');
         navigator.geolocation.getCurrentPosition(function(position) {
             const lat = position.coords.latitude;
             const lon = position.coords.longitude;
-            const url = 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/f672ff13193bfcc40427a678ebfdbc71/'+lat+','+lon+'?units=si';
+            const url = prooxy+'https://api.darksky.net/forecast/'+keey+'/'+lat+','+lon+'?units=si';
             const lurl = 'https://nominatim.openstreetmap.org/reverse?format=json&lat='+lat+'&lon='+lon+'&zoom=18&addressdetails=1'
             $.getJSON(lurl, (res)=>{
                 if(res.address.city != null)
@@ -115,9 +124,11 @@ const getGeoWeather= () => {
                 else if(res.address.station != null && (res.address.town != null || res.address.city != null))
                 city.html(res.address.station+', '+res.address.country);
                 else if(res.address.attraction != null)
-        city.html(res.address.attraction+', '+res.address.state+', '+res.address.country);
+                city.html(res.address.attraction+', '+res.address.state+', '+res.address.country);
                 else if(res.address.town != null)
-                    city.html(res.address.town+', '+res.address.country);
+                city.html(res.address.town+', '+res.address.country);
+                else
+                city.html(res.display_name);
             })
             $.getJSON(url, (res)=>{
                 temp.html(parseInt(res.currently.temperature));
@@ -132,6 +143,7 @@ const getGeoWeather= () => {
                 });
                 sign.html('C');
                 condition.html(res.currently.summary);
+                predict.html(res.hourly.summary);
                 iconify(res.currently.icon);
                 humidity.html(parseFloat(res.currently.humidity)*100+'%');
             })
